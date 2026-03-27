97a1f7dc28f8462893e6fe2fa0a1fe3d6ef4bb44
Viktor Grebnev <viktor.grebnev@gmail.com>
2026-03-27 03:13:08 +0300

    chore: finalize version 0.9.1 release (stable build)

--- version.js
+++ version.js
@@ -1,10 +1,11 @@
 const VERSION = {
    version: "0.9.1",
-build: "0.9.1-dev",
-    stage: "dev",
-    date: "2026-03-26"
+build: "0.9.1",
+stage: "stable",
+date: "2026-03-27"
 };
 
 const CHANGELOG = {
-    "0.9": "stable release"
+    "0.9": "stable release",
+    "0.9.1": "worker stability, reload fixes, UI improvements"
 };
